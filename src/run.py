5834035d15a29ff3c5038b832daf8fdec36a4fed
Ezzeri Esa <ezzeri.esa@gmail.com>
2022-11-16 17:59:33 -0800

    positions - set non-pawn positions

--- src/run.py
+++ src/run.py
@@ -1,6 +1,47 @@
+from typing import DefaultDict, List, Optional, Tuple
+import collections
+
 import chess
 
 
+FILES = "abcdefgh"
+PIECES = "kqr.b.n.p.......KQR.B.N.P......."
+
+
 if __name__ == "__main__":
+    collection: DefaultDict[str, List[Tuple[int, str]]] = collections.defaultdict(list)
+    excess = []
+    positions: List[Optional[int]] = [None] * 32
+
     board = chess.Board()
-    print(board)
+
+    for i, pieces in enumerate(str(board).split("\n")):
+        for j, piece in enumerate(pieces.split(" ")):
+            if piece == ".":
+                continue
+
+            position_int = i * 8 + j
+            position_str = FILES[j] + str(i + 1)
+
+            collection[piece].append((position_int, position_str))
+
+    for non_pawn_piece in ["k", "q", "r", "b", "n", "K", "Q", "R", "B", "N"]:
+        if non_pawn_piece not in collection:
+            continue
+
+        non_pawn_index = PIECES.index(non_pawn_piece)
+
+        if non_pawn_piece in {"k", "q", "K", "Q"}:
+            positions[non_pawn_index] = collection[non_pawn_piece][0][0]
+
+        elif non_pawn_piece in {"r", "b", "n", "R", "B", "N"}:
+            non_pawn_positions = collection[non_pawn_piece]
+            non_pawn_counter = 0
+
+            for non_pawn_position in non_pawn_positions:
+                if non_pawn_counter < 2:
+                    positions[non_pawn_index + non_pawn_counter] = non_pawn_position[0]
+                    non_pawn_counter += 1
+                    continue
+
+                excess.append((non_pawn_piece, non_pawn_position))
