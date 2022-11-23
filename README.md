36e45ac529df5282139a263ddcf1d6b0fad64d59
Ezzeri Esa <ezzeri.esa@gmail.com>
2022-11-23 11:14:12 -0800

    enable switch between binary and ints

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 bitpacks chess positions
 
 [try me!](https://replit.com/@savarin/bitpacker?v=1)
+
+```shell
+python src/run.py
+```
--- src/run.py
+++ src/run.py
@@ -49,34 +49,55 @@ def convert(board: chess.Board) -> Tuple[List[int], List[int]]:
     return non_pawn_array + pawn_array, promotions_enumeration
 
 
-def expose_board(board: chess.Board) -> None:
+def expose_board(board: chess.Board, is_binary: bool) -> None:
     array, promotions_enumeration = convert(board)
+
+    if is_binary:
+        non_pawns = hex(int("".join([format(item, "06b") for item in array[:16]]), 2))
+        pawns = hex(int("".join([format(item, "06b") for item in array[16:]]), 2))
+        white_promotions_enumeration = format(promotions_enumeration[0], "#011b")
+        black_promotions_enumeration = format(promotions_enumeration[1], "#011b")
+    else:
+        non_pawns = "-".join([f"{item:{0}{2}}" for item in array[:16]])
+        pawns = "-".join([f"{item:{0}{2}}" for item in array[16:]])
+        white_promotions_enumeration = f"{promotions_enumeration[0]:{0}{3}}"
+        black_promotions_enumeration = f"{promotions_enumeration[1]:{0}{3}}"
 
     print("\n" + str(board) + "\n")
     print(
         colorama.Fore.BLUE
-        + hex(int("".join([format(item, "06b") for item in array[:16]]), 2))
+        + non_pawns
     )
     print(
         colorama.Fore.BLUE
-        + hex(int("".join([format(item, "06b") for item in array[16:]]), 2))
+        + pawns
     )
-    print(colorama.Fore.RED + format(promotions_enumeration[0], "#011b"))
-    print(colorama.Fore.RED + format(promotions_enumeration[0], "#011b") + "\n")
+    print(colorama.Fore.RED + white_promotions_enumeration)
+    print(colorama.Fore.RED + black_promotions_enumeration + "\n")
 
 
 if __name__ == "__main__":
     colorama.init(autoreset=True)
-    print("To restart the game, type 'restart'. To leave the game, type 'exit'.")
+    is_binary = True
+
+    print("""\
+To restart the game, type 'restart'.
+To switch between binary and ints, type 'switch'.
+To leave the game, type 'exit'.""")
 
     board = chess.Board()
-    expose_board(board)
+    expose_board(board, is_binary)
 
     while True:
         move = input("Please specify a move: ")
 
         if move == "exit":
             break
+
+        elif move == "switch":
+            is_binary = not is_binary
+            expose_board(board, is_binary)
+            continue
 
         elif move == "restart":
             board = chess.Board()
@@ -89,4 +110,4 @@ if __name__ == "__main__":
             print("Valid moves only, please try again.")
             continue
 
-        expose_board(board)
+        expose_board(board, is_binary)
